4a8ffc6413a8bf891c51083f6523e9efbd9d2273
Priyanka Das <42906439+MYGITHUBPRIYANKA@users.noreply.github.com>
2021-07-12 02:26:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='ecom_finder',
-      version='0.1',
+      version='1.0.1',
       description='Python based library to find any product in ecommerce site using crawling and AI (like audio, images)',
       long_description=long_description,
       long_description_content_type='text/markdown',
